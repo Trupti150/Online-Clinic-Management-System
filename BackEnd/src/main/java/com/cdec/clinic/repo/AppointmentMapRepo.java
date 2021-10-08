3c984bdfb18a46ebdd1beafdae49cc12b70114b0
Trupti <truptijadhav15@gmail.com>
2021-10-08 18:45:14 +0530

    Initial Commit

---
+++ BackEnd/src/main/java/com/cdec/clinic/repo/AppointmentMapRepo.java
@@ -0,0 +1,17 @@
+package com.cdec.clinic.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.cdec.clinic.model.Account;
+import com.cdec.clinic.model.AppointmentsMap;
+
+public interface AppointmentMapRepo extends JpaRepository<Account, Integer> {
+	@Query(value="select a from Account a where a.appointments is not EMPTY")
+	List<Account> getAllAppoit();
+
+	
+	
+}
